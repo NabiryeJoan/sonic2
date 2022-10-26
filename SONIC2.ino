c880b666ab53c04df7200cbc174ff76fd3ffe0c8
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2022-10-26 10:35:57 +0300

    sonic2

---
+++ SONIC2.ino
@@ -0,0 +1,45 @@
+/*
+ * HC-SR04 example sketch
+ *
+ * https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380
+ *
+ * by Isaac100
+ */
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  pinMode(6,OUTPUT);
+  pinMode(4,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+  if(distance <= 30){
+    digitalWrite(6,HIGH);
+  }else if(distance > 40 && distance <= 100){
+    digitalWrite(4,HIGH);
+  }else{
+    digitalWrite(6,LOW);
+    digitalWrite(4,LOW);
+    
+  }
+  
+}
